ad9329cc8645528de747aaf6d4fba9fe926118c7
avierkin03 <avierkin03@gmail.com>
2025-09-19 15:26:59 +0300

    added requirements.txt and Docker file

--- resume_builder/settings.py
+++ resume_builder/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ipft(&lh6s41ejbzf+ad@i+l7jhnb7qwt=_m=f9)@k+48$9xi@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,6 +125,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
